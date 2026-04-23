eb9842f2a7c37c67e66a95d0694ad0b077b3db44
Anthony Labarre <48905616+alabarre@users.noreply.github.com>
2026-04-23 21:29:34 +0200

    more hole-based recognizers

--- src/graph_recognition/recognizers_exponential.py
+++ src/graph_recognition/recognizers_exponential.py
@@ -1,7 +1,7 @@
 """
 Anthony Labarre © 2026
 
-This file contains recognizers with an exponential running time.
+This file contains recognizers with a worst-case exponential running time.
 
 """
 # Imports -----------------------------------------------------------------------------------------
@@ -13,16 +13,133 @@ from functools import lru_cache
 import networkx as nx
 
 # ----- My imports --------------------------------------------------------------------------------
-from graph_recognition.fisc_based_recognizers import is_bull_free, is_diamond_free
-from graph_recognition.profitable_hereditary_n import is_planar, is_line, is_bipartite, is_cograph, is_chordal
+from graph_recognition.fisc_based_recognizers import is_bull_free, is_diamond_free, is_gc_180, is_gc_574
+from graph_recognition.profitable_hereditary_n import is_planar, is_line, is_bipartite, is_cograph, is_chordal, \
+    is_co_bipartite
 from graph_recognition.profitable_hereditary_n_2 import is_comparability
-from graph_recognition.profitable_hereditary_n_3 import is_paw_free
-from graph_recognition.profitable_hereditary_n_4 import is_c4_free, is_k4_free, is_claw_free
+from graph_recognition.profitable_hereditary_n_3 import is_paw_free, is_triangle_free, is_3k1_free
+from graph_recognition.profitable_hereditary_n_4 import is_c4_free, is_k4_free, is_claw_free, is_hole_free
+from graph_recognition.recognizers_n_4 import is_pretty
 from graph_recognition.recognizers_n_5 import is_split_neighbourhood
 from graph_recognition.recognizers_utils import current_module_recognizers, assign_class_id, assign_inherited_fisc
+from graph_recognition.subgraphs import is_h_free
 
 
 # Recognizers -------------------------------------------------------------------------------------
+# ----- Subclasses of hole-free graphs ------------------------------------------------------------
+# ---------- Subclasses of even-hole-free graphs --------------------------------------------------
+@lru_cache(maxsize=None)
+@assign_class_id("gc_547")
+def is_even_hole_free(graph: nx.Graph):
+    """
+    Even-hole-free graphs contain no induced cycles whose number of vertices is even and >= 6.
+
+    https://www.graphclasses.org/classes/gc_547.html
+
+    :param graph:
+    :return:
+    """
+    return is_hole_free(graph) or not any(
+        len_c >= 6 and not len_c % 2 for len_c in map(len, nx.chordless_cycles(graph))  # noqa
+    )
+
+
+@lru_cache(maxsize=None)
+@assign_class_id("gc_1325")
+def is_gc_1325(graph: nx.Graph):
+    """
+
+    https://www.graphclasses.org/classes/gc_1325.html
+
+    :param graph:
+    :return:
+    """
+    return is_bipartite(graph) and is_h_free(graph, [
+        "T_{2}", "X_{205}", "X_{206}", "X_{207}", "X_{208}"
+    ]) and is_even_hole_free(graph)
+
+
+# ---------- Subclasses of odd-hole-free graphs --------------------------------------------------
+@lru_cache(maxsize=None)
+@assign_class_id("gc_356")
+def is_odd_hole_free(graph: nx.Graph):
+    """
+    Odd-hole-free graphs contain no induced cycles whose number of vertices is odd and >= 5.
+
+    https://www.graphclasses.org/classes/gc_356.html
+
+    :param graph:
+    :return:
+    """
+    return is_hole_free(graph) or not any(
+        len_c >= 5 and len_c % 2 for len_c in map(len, nx.chordless_cycles(graph))  # noqa
+    )
+
+
+@lru_cache(maxsize=None)
+@assign_class_id("gc_610")
+def is_odd_hole_free_and_pretty(graph: nx.Graph):
+    """
+
+    https://www.graphclasses.org/classes/gc_610.html
+
+    :param graph:
+    :return:
+    """
+    return is_pretty(graph) and is_odd_hole_free(graph)
+
+
+@lru_cache(maxsize=None)
+@assign_class_id("gc_711")
+def is_gc_711(graph: nx.Graph):
+    """
+
+    https://www.graphclasses.org/classes/gc_711
+
+    :param graph:
+    :return:
+    """
+    return is_gc_180(graph) and is_odd_hole_free(graph)
+
+
+@lru_cache(maxsize=None)
+@assign_class_id("gc_965")
+def is_co3k2_paw_odd_hole_free(graph: nx.Graph):
+    """
+
+    https://www.graphclasses.org/classes/gc_965
+
+    :param graph:
+    :return:
+    """
+    return is_paw_free(graph) and is_h_free(graph, ["co(3K_{2})"]) and is_odd_hole_free(graph)
+
+@lru_cache(maxsize=None)
+@assign_class_id("gc_1258")
+def is_coc7_paw_odd_hole_free(graph: nx.Graph):
+    """
+
+    https://www.graphclasses.org/classes/gc_1258
+
+    :param graph:
+    :return:
+    """
+    return is_h_free(graph, ["co(C_{7})"]) and is_odd_hole_free(graph)
+
+
+@lru_cache(maxsize=None)
+@assign_class_id("gc_575")
+def is_bull_house_odd_hole_free(graph: nx.Graph):
+    """
+
+    https://www.graphclasses.org/classes/gc_575
+
+    :param graph:
+    :return:
+    """
+    return is_gc_574(graph) and is_odd_hole_free(graph)
+
+
 # ----- Subclasses of perfect graphs --------------------------------------------------------------
 @lru_cache(maxsize=None)
 @assign_class_id("gc_56")
@@ -36,17 +153,43 @@ def is_perfect(graph: nx.Graph) -> bool:
     :param graph:
     :return:
     """
+    # Strong Perfect Graph Theorem (SPGT): https://en.wikipedia.org/wiki/Strong_perfect_graph_theorem
+    # G is perfect iff it has no odd hole (an odd-length induced cycles of length at least 5) and
+    # no odd anti-hole (complement of an odd hole).
+
+    # this is exactly the characterization used in nx.is_perfect_graph(), which leads to an
+    # exponential running time. Therefore, before we consider running it, we will look for
+    # sufficient and necessary conditions for a graph to be perfect that can be checked quickly
+
+    # note that a polynomial-time recognition exists, at least in theory; I haven't come across
+    # an implementation yet. See https://www.graphclasses.org/classes/gc_56 for more details
+
+    # 1) known subclasses of perfect graphs
+
     # the following families are included in the class of perfect graphs and membership is much
     # cheaper to check; ordered by increasing complexity
     profitable_recognizers_for_subclasses = (
-        is_bipartite, is_cograph, is_chordal, is_comparability
+        is_bipartite, is_cograph, is_chordal, is_co_bipartite, is_comparability
     )
     if any(recognizer(graph) for recognizer in profitable_recognizers_for_subclasses):
         return True
 
-    # note: this algorithm runs in exponential time, but a polynomial-time algorithm exists for
-    # recognizing perfect graph (check https://www.graphclasses.org/classes/gc_56.html for more
-    # info); I haven't found an implementation of that algorithm
+    # 2) contradictions for SPGT
+
+    # none of the above worked, let's test sufficient conditions to conclude that the graph is NOT
+    # perfect. I'm repeating some of the tests I've done above to avoid regressions later should we
+    # modify what we did before; since all results are cached, calling a recognizer multiple times
+    # is cheap
+    if not is_bipartite(graph) and is_triangle_free(graph):
+        # then graph contains an odd cycle of length >= 5, which contradicts perfectness
+        return False
+
+    # same condition as above but on the complement:
+    if not is_co_bipartite(graph) and is_3k1_free(graph):
+        # then complement contains an odd cycle of length >= 5, which contradicts perfectness
+        return False
+
+    # 3) no way around it: run the exponential algorithm and hope that it finishes "quickly"
     return nx.is_perfect_graph(graph)  # noqa (pycharm can't find is_perfect_graph)
 
 
