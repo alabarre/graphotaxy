dcf3b62873dfad021fac6a7076f0ad66edd70ec2
Anthony Labarre <48905616+alabarre@users.noreply.github.com>
2026-04-23 11:06:24 +0200

    faster is_perfect replaced nx.is_perfect_graph with is_perfect everywhere

--- src/graph_recognition/recognizers_exponential.py
+++ src/graph_recognition/recognizers_exponential.py
@@ -14,7 +14,8 @@ import networkx as nx
 
 # ----- My imports --------------------------------------------------------------------------------
 from graph_recognition.fisc_based_recognizers import is_bull_free, is_diamond_free
-from graph_recognition.profitable_hereditary_n import is_planar, is_line
+from graph_recognition.profitable_hereditary_n import is_planar, is_line, is_bipartite, is_cograph, is_chordal
+from graph_recognition.profitable_hereditary_n_2 import is_comparability
 from graph_recognition.profitable_hereditary_n_3 import is_paw_free
 from graph_recognition.profitable_hereditary_n_4 import is_c4_free, is_k4_free, is_claw_free
 from graph_recognition.recognizers_n_5 import is_split_neighbourhood
@@ -22,6 +23,7 @@ from graph_recognition.recognizers_utils import current_module_recognizers, assi
 
 
 # Recognizers -------------------------------------------------------------------------------------
+# ----- Subclasses of perfect graphs --------------------------------------------------------------
 @lru_cache(maxsize=None)
 @assign_class_id("gc_56")
 def is_perfect(graph: nx.Graph) -> bool:
@@ -34,6 +36,14 @@ def is_perfect(graph: nx.Graph) -> bool:
     :param graph:
     :return:
     """
+    # the following families are included in the class of perfect graphs and membership is much
+    # cheaper to check; ordered by increasing complexity
+    profitable_recognizers_for_subclasses = (
+        is_bipartite, is_cograph, is_chordal, is_comparability
+    )
+    if any(recognizer(graph) for recognizer in profitable_recognizers_for_subclasses):
+        return True
+
     # note: this algorithm runs in exponential time, but a polynomial-time algorithm exists for
     # recognizing perfect graph (check https://www.graphclasses.org/classes/gc_56.html for more
     # info); I haven't found an implementation of that algorithm
@@ -52,7 +62,7 @@ def is_perfect_and_planar(graph: nx.Graph) -> bool:
     :param graph:
     :return:
     """
-    return is_planar(graph) and nx.is_perfect_graph(graph)  # noqa (pycharm can't find is_perfect_graph)
+    return is_planar(graph) and is_perfect(graph)
 
 
 @lru_cache(maxsize=None)
@@ -64,7 +74,7 @@ def is_perfect_and_split_neighbourhood(graph: nx.Graph) -> bool:
     :param graph:
     :return:
     """
-    return (is_split_neighbourhood(graph) and nx.is_perfect_graph(graph))  # noqa (pycharm can't find is_perfect_graph)
+    return is_split_neighbourhood(graph) and is_perfect(graph)
 
 
 @assign_inherited_fisc()
@@ -78,7 +88,7 @@ def is_c4_free_and_perfect(graph: nx.Graph) -> bool:
     :param graph:
     :return:
     """
-    return (is_c4_free(graph) and nx.is_perfect_graph(graph))  # noqa (pycharm can't find is_perfect_graph)
+    return is_c4_free(graph) and is_perfect(graph)
 
 
 @assign_inherited_fisc()
@@ -92,7 +102,7 @@ def is_k4_free_and_perfect(graph: nx.Graph) -> bool:
     :param graph:
     :return:
     """
-    return (is_k4_free(graph) and nx.is_perfect_graph(graph))  # noqa (pycharm can't find is_perfect_graph)
+    return is_k4_free(graph) and is_perfect(graph)
 
 
 @assign_inherited_fisc()
@@ -106,7 +116,7 @@ def is_bull_free_and_perfect(graph: nx.Graph) -> bool:
     :param graph:
     :return:
     """
-    return (is_bull_free(graph) and nx.is_perfect_graph(graph))  # noqa (pycharm can't find is_perfect_graph)
+    return is_bull_free(graph) and is_perfect(graph)
 
 
 @assign_inherited_fisc()
@@ -120,8 +130,7 @@ def is_claw_free_and_perfect(graph: nx.Graph) -> bool:
     :param graph:
     :return:
     """
-    return (is_claw_free(graph)
-            and nx.is_perfect_graph(graph))  # noqa (pycharm can't find is_perfect_graph)
+    return is_claw_free(graph) and is_perfect(graph)
 
 
 @assign_inherited_fisc()
@@ -135,8 +144,7 @@ def is_diamond_free_and_perfect(graph: nx.Graph) -> bool:
     :param graph:
     :return:
     """
-    return (is_diamond_free(graph)
-            and nx.is_perfect_graph(graph))  # noqa (pycharm can't find is_perfect_graph)
+    return is_diamond_free(graph) and is_perfect(graph)
 
 
 @assign_inherited_fisc()
@@ -150,7 +158,7 @@ def is_line_and_perfect(graph: nx.Graph) -> bool:
     :param graph:
     :return:
     """
-    return (is_line(graph) and nx.is_perfect_graph(graph))  # noqa (pycharm can't find is_perfect_graph)
+    return is_line(graph) and is_perfect(graph)
 
 
 @assign_inherited_fisc()
@@ -165,7 +173,7 @@ def is_line_and_perfect(graph: nx.Graph) -> bool:
     :param graph:
     :return:
     """
-    return nx.is_perfect_graph(nx.line_graph(graph))  # noqa (pycharm can't find is_perfect_graph)
+    return is_perfect(nx.line_graph(graph))
 
 
 @assign_inherited_fisc()
@@ -179,7 +187,7 @@ def is_paw_free_and_perfect(graph: nx.Graph) -> bool:
     :param graph:
     :return:
     """
-    return (is_paw_free(graph) and nx.is_perfect_graph(graph))  # noqa (pycharm can't find is_perfect_graph)
+    return is_paw_free(graph) and is_perfect(graph)
 
 
 # This code segment must always be at the END of a recognizer file --------------------------------
